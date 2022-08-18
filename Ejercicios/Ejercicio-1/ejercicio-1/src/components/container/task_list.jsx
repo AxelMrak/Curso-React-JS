430350f2484d9198ae8e716cc13b7fcaef0c54b3
AxelMrak <axelmrak@gmail.com>
2022-08-18 19:43:52 -0300

    Add session 5

--- Ejercicios/Ejercicio-1/ejercicio-1/src/components/container/task_list.jsx
+++ Ejercicios/Ejercicio-1/ejercicio-1/src/components/container/task_list.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { LEVELS } from '../../models/level.enum';
 import { Task } from '../../models/task.class';
 import TaskComponent from '../pure/task';
 
 const TaskListComponent = () => { //Cuando declaremos este componente se ejecutara esta funcion. Cuando React encuentre este component lanzara la funcion.
 
+//Estado del componente
+    const [tasks, setTasks] = useState({defaultTask});
+    const [loading, setLoading] = useState(true); //Estado de carga
+
+    //Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log(`Task state has been modified`) //Al ser actualizado
+        setLoading(false); //Al ser actualizado no esta cargado
+      return () => {
+        console.log`TaskList component is going to unmount`; //Al ser desmontado
+      }
+    }, [tasks])
+    
+
+
+
+    
     const defaultTask = new Task(`Example`,  `Default description`, false, LEVELS.NORMAL); //Creamos una instancia de la clase TASK. Le pasamos las propiedades que ya declaramos en la clase. VsCode lo marca automaticamente tambien que debemos estar pasandole. Como levels es un objeto, con el punto accedemos a su propiedad (Aclaro a mi yo futuro)
 
-    const changeState = id => {
+    const changeCompleted = id => {
         console.log(`TODO: Cambiara el estado de una tarea. Para la proxima clase`)
     };
 
+    <div>
     return (
-        <div>
             <div>
                 Your tasks: 
             </div>
--- Ejercicios/Ejercicio-1/ejercicio-1/src/components/pure/task.jsx
+++ Ejercicios/Ejercicio-1/ejercicio-1/src/components/pure/task.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Task } from '../../models/task.class'
- 
+
 function TaskComponent({ task }) { //Le pasamos TaskComponent en vez de props.
-  return (
-    <div>
-        <h2>
-            Name: { task.name } 
-            {/* Vamos sacando las propiedades de task y las pintamos en el div padre */}
-        </h2>
-        <h3>
-            Description: { task.description }
-        </h3>
-        <h4>
-            Level: { task.level }
-        </h4>
-        <h5>
-            This task is: { task.completed ? `Completed` : `Pending` } 
-            {/* Lo anterior es renderizado condicional, utilizando el operador ternario, le decimos que si la tarea esta completa haga algo y sino que haga otra cosa. Incluso podriamos pasarle otra variable y hacerlo reactivo a lo que suceda en otro lugar.  */}
-        </h5>
-    </div>
-  )
+
+
+    useEffect(() => {
+
+        console.log(`Created Task`)
+
+        return () => {
+            console.log(`Task: ${task.name} is going to unmount`)
+        }
+    }, [task])
+
+
+
+
+    return (
+        <div>
+            <h2>
+                Name: {task.name}
+                {/* Vamos sacando las propiedades de task y las pintamos en el div padre */}
+            </h2>
+            <h3>
+                Description: {task.description}
+            </h3>
+            <h4>
+                Level: {task.level}
+            </h4>
+            <h5>
+                This task is: {task.completed ? `Completed` : `Pending`}
+                {/* Lo anterior es renderizado condicional, utilizando el operador ternario, le decimos que si la tarea esta completa haga algo y sino que haga otra cosa. Incluso podriamos pasarle otra variable y hacerlo reactivo a lo que suceda en otro lugar.  */}
+            </h5>
+        </div>
+    )
 }
 
 TaskComponent.propTypes = {
