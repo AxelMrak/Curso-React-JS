96514e2dd26720b1b05328c0b6763cd8d1e0387e
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2022-08-23 21:22:44 -0300

    Practicas 1 Modified

--- Ejercicios/Proyecto evolutivo/project-evolutive/src/components/container/father.jsx
+++ Ejercicios/Proyecto evolutivo/project-evolutive/src/components/container/father.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Child from '../pure/child';
 
 const Father = () => {
+
+  const [name, setName] = useState(`Axel`)
+
+  const showMessage = (text) => {  //Metodo a ser ejecutado por el hijo pero controlado por el padre.
+    alert(`Message received: ${text}`)
+  };
+
+  const updateName = newName => {
+    setName(newName)
+  };
+
   return (
-    <div>
-        <Child></Child>
+    <div style={{background: 'tomato', padding: '1rem'}}>
+        <Child name={name} send={showMessage} update={updateName}></Child>
     </div>
   )
 };
